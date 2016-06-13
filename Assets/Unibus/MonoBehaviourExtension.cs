557a2587053c197239f3105728f18bee8ec8e17d
mattak <mattak.me@gmail.com>
2016-06-14 01:13:49 +0900

    feat(UnibusEnableSubscriber): created

---
+++ Assets/Unibus/MonoBehaviourExtension.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Unibus
+{
+    public static class MonoBehaviourExtension
+    {
+        public static void BindEnableTo<T>(this MonoBehaviour mono, OnEvent<T> onEvent)
+        {
+            var component = mono.GetComponent<UnibusEnableScriber>();
+
+            if (null == component)
+            {
+                component = mono.gameObject.AddComponent<UnibusEnableScriber>();
+            }
+
+            component.SetSubscribeCaller(onEvent);
+        }
+    }
+}
---
+++ Assets/Unibus/UnibusEnableScriber.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+namespace Unibus
+{
+    public class UnibusEnableScriber : MonoBehaviour
+    {
+        private Action<bool> subscribeCaller;
+
+        public void SetSubscribeCaller<T>(OnEvent<T> onEvent)
+        {
+            subscribeCaller = (bool active) =>
+                {
+                    if (active)
+                    {
+                        Unibus.Bus.Instance.Subscribe(onEvent);
+                    }
+                    else
+                    {
+                        Unibus.Bus.Instance.Unsubscribe(onEvent);
+                    }
+                };
+            subscribeCaller(true);
+        }
+
+        void OnEnable()
+        {
+            if (subscribeCaller != null)
+            {
+                subscribeCaller(true);
+            }
+        }
+
+        void OnDisable()
+        {
+            if (subscribeCaller != null)
+            {
+                subscribeCaller(false);
+            }
+        }
+    }
+}
