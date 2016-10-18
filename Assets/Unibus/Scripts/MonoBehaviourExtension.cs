c25baf86c64893ce3fb058c65c6ee106954e8ee6
mattak <mattak.me@gmail.com>
2016-10-18 09:27:33 +0900

    issues/6 refactor(rename): Unibus{Enable,Sustain}Subscriber => Unibus{Disable,Destroy}Subscriber

--- Assets/Unibus/Scripts/MonoBehaviourExtension.cs
+++ Assets/Unibus/Scripts/MonoBehaviourExtension.cs
@@ -7,22 +7,22 @@ namespace Unibus
     {
         public static void BindUntilDisable<T>(this MonoBehaviour mono, OnEvent<T> onEvent)
         {
-            GetOrAddComponent<T, UnibusEnableSubscriber>(mono, UnibusEventObject.DefaultTag, onEvent);
+            GetOrAddComponent<T, UnibusDisableSubscriber>(mono, UnibusEventObject.DefaultTag, onEvent);
         }
 
         public static void BindUntilDisable<T>(this MonoBehaviour mono, object tag, OnEvent<T> onEvent)
         {
-            GetOrAddComponent<T, UnibusEnableSubscriber>(mono, tag, onEvent);
+            GetOrAddComponent<T, UnibusDisableSubscriber>(mono, tag, onEvent);
         }
 
         public static void BindUntilDestroy<T>(this MonoBehaviour mono, OnEvent<T> onEvent)
         {
-            GetOrAddComponent<T, UnibusSustainSubscriber>(mono, UnibusEventObject.DefaultTag, onEvent);
+            GetOrAddComponent<T, UnibusDestroySubscriber>(mono, UnibusEventObject.DefaultTag, onEvent);
         }
 
         public static void BindUntilDestroy<T>(this MonoBehaviour mono, object tag, OnEvent<T> onEvent)
         {
-            GetOrAddComponent<T, UnibusSustainSubscriber>(mono, tag, onEvent);
+            GetOrAddComponent<T, UnibusDestroySubscriber>(mono, tag, onEvent);
         }
 
         private static void GetOrAddComponent<T, S>(MonoBehaviour mono, object tag, OnEvent<T> onEvent) where S : UnibusSubscriberBase
